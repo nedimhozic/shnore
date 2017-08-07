443fb914054c19b2743a028b3602b2f83ccb3ff9
nedim <nedimhozic@gmail.com>
2017-08-07 18:31:07 +0200

    Crypting data and help

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { TriggerService } from './common/trigger.service';
 import { ModalType } from './modal/modal-type.enum';
+import { DatePipe } from '@angular/common';
+import { CookieService } from 'angular2-cookie/core';
 
 @Component({
   selector: 'app-root',
@@ -8,11 +10,25 @@ import { ModalType } from './modal/modal-type.enum';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  constructor(private trigger: TriggerService){
+  public today: Date = new Date();
+  public showHelp: boolean = false;
+  public firstVisitKey = 'firstVisit_636C8F1B4A654D5FA2183348F6E2F8E9';
 
+  constructor(
+    private trigger: TriggerService,
+    private cookie: CookieService) {
+      let firstVisitCookie = this.cookie.get(this.firstVisitKey);
+      if(!firstVisitCookie) {
+        this.showHelp = true;
+        this.cookie.put(this.firstVisitKey, 'shnore');
+      }
   }
 
-  openModal(){
+  openModal() {
     this.trigger.openModal(ModalType.Set);
+  }
+
+  closeHelp() {
+    this.showHelp = false;
   }
 }
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -7,15 +7,18 @@ import { HttpModule } from '@angular/http';
 import { AppComponent } from './app.component';
 import { NoteComponent } from './note/note.component';
 import { ModalComponent } from './modal/modal.component';
+import { HelpComponent } from './help/help.component';
 
 import { BaseService } from './common/base.service';
 import { TriggerService } from './common/trigger.service';
+import { CookieService } from 'angular2-cookie/services/cookies.service';
 
 @NgModule({
   declarations: [
     AppComponent,
     NoteComponent,
-    ModalComponent
+    ModalComponent,
+    HelpComponent
   ],
   imports: [
     BrowserModule,
@@ -24,7 +27,7 @@ import { TriggerService } from './common/trigger.service';
     ReactiveFormsModule,
     HttpModule
   ],
-  providers: [BaseService, TriggerService],
+  providers: [BaseService, TriggerService, CookieService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
