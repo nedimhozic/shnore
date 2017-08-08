0cc492ef94c6bb74a9246f5ba9747b353743cfa5
nedim <nedimhozic@gmail.com>
2017-08-09 00:25:51 +0200

    Change crypting way

--- server/helpers/crypt.js
+++ server/helpers/crypt.js
@@ -1,8 +1,9 @@
 var bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
+var crypto = require('crypto');
 var config = require('../config');
 
-module.exports.hash = function(password, callback) {
+module.exports.hash = function (password, callback) {
     bcrypt.genSalt(10, function (err, salt) {
         bcrypt.hash(password, salt, function (err, hash) {
             callback(err, hash);
@@ -10,18 +11,22 @@ module.exports.hash = function(password, callback) {
     });
 }
 
-module.exports.compare = function(candidatePassword, hash, callback) {
+module.exports.compare = function (candidatePassword, hash, callback) {
     bcrypt.compare(candidatePassword, hash, callback);
 }
 
 
 module.exports.encode = function (content) {
-    return jwt.sign(content, config.contentSecret);
+    var cipher = crypto.createCipher(config.algorithm, config.contentPassword)
+    var crypted = cipher.update(content, 'utf8', 'hex')
+    crypted += cipher.final('hex');
+    return crypted;
 }
 
 module.exports.decode = function (content) {
-    if(!content) return '';
-    var result = jwt.verify(content, config.contentSecret);
-    return result;
+    var decipher = crypto.createDecipher(config.algorithm, config.contentPassword)
+    var dec = decipher.update(content, 'hex', 'utf8')
+    dec += decipher.final('utf8');
+    return dec;
 }
 
